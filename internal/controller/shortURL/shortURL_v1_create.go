8b2abbce6dc959b5a13aa33ed8f184232af47411
zcj <18137693952@163.com>
2024-02-27 22:28:39 +0800

    feat(shortURL): 创建短链时填充 用户 id

--- internal/controller/shortURL/shortURL_v1_create.go
+++ internal/controller/shortURL/shortURL_v1_create.go
@@ -9,10 +9,17 @@ import (
 )
 
 func (c *ControllerV1) Create(ctx context.Context, req *v1.CreateReq) (res *v1.CreateRes, err error) {
+	// 获取当前登陆用户信息
+	loginUserInfo, err := service.Auth().GetLoginUserInfo(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	info := entity.ShortUrl{
 		RawUrl:         req.RawUrl,
 		Title:          req.Title,
 		GroupId:        req.GroupId,
+		UserId:         loginUserInfo.Id,
 		ExpirationTime: gtime.NewFromStrFormat(req.ExpirationTime, "Y-m-d H:i:s"),
 	}
 
