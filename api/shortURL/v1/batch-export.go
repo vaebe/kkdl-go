6397c7b792be3101cdeb80ef9f7596b38337d0d2
zcj <18137693952@163.com>
2024-02-29 21:35:22 +0800

    fix(shortURL): 接口路径

--- api/shortURL/v1/batch-export.go
+++ api/shortURL/v1/batch-export.go
@@ -6,7 +6,7 @@ import (
 
 // BatchExportReq 批量导出
 type BatchExportReq struct {
-	g.Meta `path:"/common/batchExport" method:"post" summary:"批量导出" tags:"短链"`
+	g.Meta `path:"/shortURL/batchExport" method:"post" summary:"批量导出" tags:"短链"`
 	Title  string `json:"title"   dc:"短链标题"`
 	RawUrl string `json:"rawUrl"   dc:"跳转链接"`
 }
--- api/shortURL/v1/template-download.go
+++ api/shortURL/v1/template-download.go
@@ -6,7 +6,7 @@ import (
 
 // TemplateDownloadReq 导入模版下载
 type TemplateDownloadReq struct {
-	g.Meta `path:"/common/templateDownload" method:"get" summary:"导入模版下载" tags:"短链"`
+	g.Meta `path:"/shortURL/templateDownload" method:"get" summary:"导入模版下载" tags:"短链"`
 }
 
 type TemplateDownloadRes struct {
