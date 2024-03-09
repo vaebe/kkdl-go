dd552aea98de74d4f17b5707dad66480d88504e8
zcj <18137693952@163.com>
2024-03-09 16:03:01 +0800

    feat(common): 发送验证码接口增加用户已注册,验证码是否过期校验

--- internal/controller/common/common_v1_get_verification_code_email.go
+++ internal/controller/common/common_v1_get_verification_code_email.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"compressURL/api/common/v1"
+	v12 "compressURL/api/user/v1"
 	"compressURL/internal/service"
 	"context"
 	"github.com/gogf/gf/v2/errors/gerror"
@@ -10,9 +11,30 @@ import (
 )
 
 func (c *ControllerV1) GetVerificationCodeEmail(ctx context.Context, req *v1.GetVerificationCodeEmailReq) (res *v1.GetVerificationCodeEmailRes, err error) {
-	code := grand.S(6)
+	// 检查用户是否已经注册
+	_, total, err := service.User().GetUserList(ctx, v12.GetListReq{Email: req.Email})
+	if err != nil {
+		return nil, err
+	}
 
-	err = g.Redis().SetEX(ctx, req.Email, code, 60*10)
+	if total != 0 {
+		return nil, gerror.New("用户已注册请直接登录!")
+	}
+
+	// 检查验证码是否过期
+	ttl, err := g.Redis().TTL(ctx, req.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	// ttl -1 没有设置过期时间,-2 键已经过期或不存在
+	if ttl != -1 && ttl != -2 {
+		return nil, gerror.Newf("请勿重复请求,有效期还剩余 %d 秒!", ttl)
+	}
+
+	// 发送验证码
+	code := grand.S(6)
+	err = g.Redis().SetEX(ctx, req.Email, code, 60*2)
 	if err != nil {
 		return nil, gerror.New("redis 缓存邮箱验证码失败!")
 	}
