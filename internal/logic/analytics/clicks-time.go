cc5127cb5115c8aa20bff9912c3c55f85a7f8d11
vaebe <18137693952@163.com>
2024-05-23 23:06:10 +0800

    feat(analytics): 根据根据日期统计

--- internal/logic/analytics/clicks-time.go
+++ internal/logic/analytics/clicks-time.go
@@ -16,7 +16,6 @@ func statisticsByHour(ctx context.Context, shortUrl string) (list v1.ClicksTimeR
 
 	// 生成唯一的临时表名
 	hoursTableName := fmt.Sprintf("temp_hours_%s", grand.Digits(6))
-	g.Log().Debug(ctx, hoursTableName)
 
 	_, err = g.DB().Exec(ctx, fmt.Sprintf(`CREATE TEMPORARY TABLE %s (hour INT PRIMARY KEY);`, hoursTableName))
 	if err != nil {
@@ -80,15 +79,68 @@ func statisticsByHour(ctx context.Context, shortUrl string) (list v1.ClicksTimeR
 	return
 }
 
+// statisticsByDays 根据传入天数进行统计
+func statisticsByDays(ctx context.Context, shortUrl string, day int) (list v1.ClicksTimeRes, err error) {
+	startDate := gtime.Now().AddDate(0, 0, day)
+	endDate := gtime.Now() // 今天
+
+	// 原始SQL语句
+	sql := `
+		SELECT
+			d.Date AS date,
+			COUNT(suv.id) AS visit_count
+		FROM (
+			SELECT a.Date
+			FROM (
+				SELECT CURDATE() - INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY AS Date
+				FROM (SELECT 0 AS a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) a
+				CROSS JOIN (SELECT 0 AS a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) b
+				CROSS JOIN (SELECT 0 AS a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) c
+			) a
+			WHERE a.Date BETWEEN ? AND ?
+			ORDER BY a.Date
+		) d
+		LEFT JOIN
+		short_url_visits suv
+		ON DATE(suv.created_at) = d.Date
+		AND (? = '' OR suv.short_url = ?)
+		GROUP BY
+		d.Date
+		ORDER BY
+		d.Date;
+	`
+
+	// 执行原始SQL查询
+	rows, err := g.DB().GetAll(ctx, sql, startDate.String(), endDate.String(), shortUrl, shortUrl)
+	if err != nil {
+		g.Log().Error(ctx, "根据日期统计失败", err)
+	}
+
+	// 处理结果
+	for _, row := range rows {
+		list = append(list, v1.ClicksTimeItem{
+			Clicks: row["visit_count"].Int(),
+			Time:   row["date"].String(),
+		})
+	}
+
+	g.Log().Info(ctx, len(list))
+
+	return
+}
+
 // GetVisitsByDate 根据时间统计访问数据
 func (s *sAnalytics) GetVisitsByDate(ctx context.Context, req v1.ClicksTimeReq) (list v1.ClicksTimeRes, err error) {
 	if req.DateType == "24h" {
-		list, err = statisticsByHour(ctx, req.Code)
-		if err != nil {
-			return nil, err
-		}
+		return statisticsByHour(ctx, req.Code)
+	}
 
-		return list, err
+	if req.DateType == "7d" {
+		return statisticsByDays(ctx, req.Code, -7)
+	}
+
+	if req.DateType == "30d" {
+		return statisticsByDays(ctx, req.Code, -30)
 	}
 
 	return
