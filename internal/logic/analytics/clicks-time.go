24fc4c7c20acb1de8b2aa148a1f8ee804b61d5ad
vaebe <18137693952@163.com>
2024-06-06 22:38:03 +0800

    feat(analytics): 根据时间统计优化

--- internal/logic/analytics/clicks-time.go
+++ internal/logic/analytics/clicks-time.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gtime"
-	"github.com/gogf/gf/v2/util/grand"
 	"golang.org/x/net/context"
 )
 
@@ -15,66 +14,44 @@ func statisticsByHour(ctx context.Context, shortUrl string) (list v1.ClicksTimeR
 	startDate := now.StartOfDay()
 	endDate := now.EndOfDay()
 
-	// 生成唯一的临时表名
-	hoursTableName := fmt.Sprintf("temp_hours_%s", grand.Digits(6))
+	// 创建查询对象
+	db := g.DB().Model("short_url_visits").
+		Fields("HOUR(created_at) AS hour, COUNT(id) AS visit_count").
+		Where("created_at BETWEEN ? AND ?", startDate, endDate).
+		Group("HOUR(created_at)").
+		Order("HOUR(created_at)")
 
-	_, err = g.DB().Exec(ctx, fmt.Sprintf(`CREATE TEMPORARY TABLE %s (hour INT PRIMARY KEY);`, hoursTableName))
-	if err != nil {
-		g.Log().Error(ctx, "短链访问根据时间统计创建临时表失败", err)
-		return
-	}
-
-	// 创建临时表
-	sql := fmt.Sprintf(`INSERT INTO %s (hour) VALUES (0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11),(12), (13), (14), (15), (16), (17), (18), (19), (20), (21), (22), (23);`, hoursTableName)
-	_, err = g.DB().Exec(ctx, sql)
-	if err != nil {
-		g.Log().Error(ctx, "短链访问根据时间统计 hours 临时表插入数据失败", err)
-		return
-	}
-
-	// 构建查询
-	query := fmt.Sprintf(`
-	SELECT 
-		h.hour,
-		COUNT(suv.id) AS visit_count
-	FROM 
-		%s h
-	LEFT JOIN 
-		short_url_visits suv 
-		ON HOUR(suv.created_at) = h.hour
-		AND suv.created_at BETWEEN ? AND ?
-	`, hoursTableName)
-	args := []interface{}{startDate.String(), endDate.String()}
-
-	// 如果shortUrl不为空，添加条件
+	// 如果 shortUrl 不为空，添加条件
 	if shortUrl != "" {
-		query += " AND suv.short_url = ?"
-		args = append(args, shortUrl)
+		db = db.Where("short_url", shortUrl)
 	}
-
-	query += " GROUP BY h.hour ORDER BY h.hour;"
 
 	// 执行查询
-	rows, err := g.DB().GetAll(ctx, query, args...)
+	rows, err := db.All()
 	if err != nil {
 		g.Log().Error(ctx, "短链访问根据时间统计 查询数据失败", err)
-
 		return
+	}
+
+	// 初始化每小时点击数为0
+	hourlyClicks := make(map[int]int)
+	for i := 0; i < 24; i++ {
+		hourlyClicks[i] = 0
 	}
 
 	// 处理结果
 	for _, row := range rows {
-		list = append(list, v1.ClicksTimeItem{
-			Clicks: row["visit_count"].Int(),
-			Time:   row["hour"].String(),
-		})
+		hour := row["hour"].Int()
+		visitCount := row["visit_count"].Int()
+		hourlyClicks[hour] = visitCount
 	}
 
-	// 删除临时表
-	_, err = g.DB().Exec(ctx, fmt.Sprintf("DROP TEMPORARY TABLE %s;", hoursTableName))
-	if err != nil {
-		g.Log().Error(ctx, "短链访问根据时间统计 删除临时表失败", err)
-		return
+	// 将结果转换为列表形式并按时间排序
+	for hour := 0; hour < 24; hour++ {
+		list = append(list, v1.ClicksTimeItem{
+			Clicks: hourlyClicks[hour],
+			Time:   fmt.Sprintf("%02d:00", hour),
+		})
 	}
 
 	return
