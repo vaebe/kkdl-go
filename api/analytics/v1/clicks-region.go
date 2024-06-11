c0a9e93bc52b49f9c9c018407985fa64e18291e4
vaebe <18137693952@163.com>
2024-06-11 21:52:22 +0800

    feat(short_url_visits): 根据区域统计 增加根据城市查询

--- api/analytics/v1/clicks-region.go
+++ api/analytics/v1/clicks-region.go
@@ -9,12 +9,14 @@ type ClicksRegionReq struct {
 	g.Meta   `path:"/analytics/clicksRegion" method:"get" summary:"根据区域统计短链访问次数" tags:"统计分析"`
 	Code     string `json:"code"   dc:"短链 code"`
 	DateType string `json:"date" dc:"日期类型"`
+	Type     string `json:"type" v:"required|in:countries,cities" dc:"类型"`
 }
 
 type ClicksRegionItem struct {
 	Clicks      int    `json:"clicks"   dc:"访问次数"`
-	CountryCode string `json:"countryCode" dc:" 国家 code"`
-	Country     string `json:"country" dc:"国家名称"`
+	Code        string `json:"code" dc:"区域 code"`
+	Name        string `json:"name" dc:"区域名称"`
+	CountryCode string `json:"countryCode" dc:"国家 code"`
 }
 
 type ClicksRegionRes []ClicksRegionItem
--- internal/logic/analytics/clicks-region.go
+++ internal/logic/analytics/clicks-region.go
@@ -3,21 +3,41 @@ package analytics
 import (
 	v1 "compressURL/api/analytics/v1"
 	"compressURL/internal/dao"
+	"github.com/gogf/gf/v2/frame/g"
 	"golang.org/x/net/context"
 )
 
 // GetVisitsByRegion 根据区域统计访问数据
 func (s *sAnalytics) GetVisitsByRegion(ctx context.Context, req v1.ClicksRegionReq, userId string) (list v1.ClicksRegionRes, err error) {
-	err = dao.ShortUrlVisits.Ctx(ctx).OmitEmptyWhere().
+	db := dao.ShortUrlVisits.Ctx(ctx).OmitEmptyWhere().
 		Where(dao.ShortUrlVisits.Columns().ShortUrl, req.Code).
-		Where(dao.ShortUrlVisits.Columns().UserId, userId).
-		Fields(dao.ShortUrlVisits.Columns().CountryCode, dao.ShortUrlVisits.Columns().Country).
-		FieldCount(dao.ShortUrlVisits.Columns().ShortUrl, "clicks").
-		Group(dao.ShortUrlVisits.Columns().CountryCode, dao.ShortUrlVisits.Columns().Country).
-		Scan(&list)
+		Where(dao.ShortUrlVisits.Columns().UserId, userId)
+
+	// 定义一个通用的处理函数
+	queryAndLog := func(fields, groupBy string) error {
+		return db.Fields(fields).
+			Group(groupBy).
+			Scan(&list)
+	}
+
+	switch req.Type {
+	case "countries":
+		err = queryAndLog(
+			"country_code, country_code AS code, country AS name, COUNT(short_url) AS clicks",
+			"country_code, country",
+		)
+	case "cities":
+		err = queryAndLog(
+			"country_code, region AS code, city AS name, COUNT(short_url) AS clicks",
+			"country_code, region, city",
+		)
+	default:
+		g.Log().Warning(ctx, "Unsupported type: ", req.Type)
+		return list, nil
+	}
 
 	if err != nil {
-		return nil, err
+		g.Log().Error(ctx, "Error executing query: ", err)
 	}
 
 	return
