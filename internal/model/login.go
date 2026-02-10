595b9a5ce407c9251686f7d408c8842b288fe419
vae <18137693952@163.com>
2026-02-10 14:58:39 +0800

    feat: 移除 wxId

--- internal/model/login.go
+++ internal/model/login.go
@@ -4,9 +4,8 @@ import "compressURL/internal/model/entity"
 
 type LoginInput struct {
 	Email       string // 邮箱,唯一
-	WxId        string // 小程序id,唯一
-	Password    string // 密码, 小程序登录无密码
-	AccountType string // 账号类型: 01 邮箱 02 小程序
+	Password    string // 密码
+	AccountType string // 账号类型: 01 邮箱
 }
 
 type LoginRes struct {
